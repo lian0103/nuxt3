236e30ef2e1c5d17764104eec2f203c46cec6fd8
Jason <gt07891@greattree.com.tw>
2022-06-27 11:37:05 +0800

    undo

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,10 +2,6 @@ import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  target: 'static',
-  router: {
-    base: '/nuxt3/'
-  },
   css: ['@/assets/css/main.css'],
   build: {
     postcss: {
