3b4c74308c78da09a44a43944ae2af9dd80d1d57
Jason <gt07891@greattree.com.tw>
2022-06-27 13:47:30 +0800

    try

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
       postcssOptions: require('./postcss.config.js'),
     },
   },
-  publicRuntimeConfig: {
-    port: 3000,
-  },
+  // publicRuntimeConfig: {
+  //   port: 3000,
+  // },
 });
